fc726995f666e6a4a9c2fc2bb17f9a8b1754db22
zhouxiaoming <1258416887@qq.com>
2019-07-30 16:52:36 +0800

    修改目录结构

--- django-rbac/urls.py
+++ django-rbac/urls.py
@@ -1,4 +1,4 @@
-"""auto_luffy URL Configuration
+"""django-rbac URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
